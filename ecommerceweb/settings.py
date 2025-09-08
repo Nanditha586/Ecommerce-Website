8bfd88a1e6d8b0de8d15645bddbaa42d712cf15f
Nanditha586 <127416252+Nanditha586@users.noreply.github.com>
2025-09-08 12:30:39 +0530

    Update settings.py

--- ecommerceweb/settings.py
+++ ecommerceweb/settings.py
@@ -7,8 +7,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "change-this-in-production"
 DEBUG = True
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "ecommerce-website-gzce.onrender.com",  
+]
 INSTALLED_APPS = [
     'django.contrib.admin','django.contrib.auth','django.contrib.contenttypes',
     'django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles',
