65fea66123bd724669befb33977b2d9efd94f2c5
Nanditha586 <127416252+Nanditha586@users.noreply.github.com>
2025-09-08 12:17:06 +0530

    Update settings.py

--- ecommerceweb/settings.py
+++ ecommerceweb/settings.py
@@ -58,9 +58,10 @@ DATABASES = {
 # Static and media
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",  # optional global static folder
+    os.path.join(BASE_DIR, 'static'),  # optional global static folder
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
